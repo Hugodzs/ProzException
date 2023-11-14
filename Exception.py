b9092175b09e1d627174a9a260abf77830e5a95d
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-11-14 11:35:50 -0300

    Update Exception.py

--- Exception.py
+++ Exception.py
@@ -1,11 +1,14 @@
+## guardei o nome do usuario
 anoCorreto = False
 nome = input("Qual seu nome completo?")
 ano = 2023
 
+##laço de repetição while, repetir até que a variavel seja "TRUE"
 while (anoCorreto == False):
 
    print("Insira um ano de nascimento entre 1922 e 2021")
 
+# Try...Expect para verifiação de erros   
    try:
 
       anoNascimento = int(input())
@@ -14,12 +17,12 @@ while (anoCorreto == False):
       if (anoNascimento >= 1922) and (anoNascimento <= 2021):
 
            anoCorreto = True
-
-           print(f"{nome} sua idade é {idade}")
+         
+           print(f"{nome} sua idade é {idade}")   #saída se estiver tudo ok
       else :
 
            print("Ano de Nascimento inválido!")
 
    except:
-
+     # caso o usuario digite um texto no campo de ano de Nascimento
        print("Texto é inválido, por favor digite um número válido")
